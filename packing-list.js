9d52046c16d06de9aa85751522d023a0273832b7
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-02-05 06:37:32 +0000

    packing

--- packing-list.js
+++ packing-list.js
@@ -4,3 +4,15 @@ console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+packingList[3];
+
+
+//ex2
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
