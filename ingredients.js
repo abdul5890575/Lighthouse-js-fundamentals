d867a6be549af4cb5fc1919e173cafd7b9878425
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-02-05 20:26:16 +0000

    ex ing

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+i=0
+while ( i < ingredients.length){
+  console.log(ingredients[i]);
+  i++;
+}
+
+for ( y=0; y<ingredients.length; y++){
+  console.log(ingredients[y]);
+}
+
+for (z=ingredients.length-1; z>=0; z--){
+  console.log(ingredients[z]);
+}
+
+
